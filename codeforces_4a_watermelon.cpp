aa043c3e3883b1a7becbaa6b3d2108221001dd23
Md. Jahidul Islam <97834703+Jahid234@users.noreply.github.com>
2022-06-10 10:43:15 +0600

    Add files via upload

---
+++ codeforces_4a_watermelon.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int w;
+    cin >> w;
+    if(w <= 2 || w %2 !=0){
+        cout << "No\n" << endl;
+    }else{
+        cout << "Yes\n" << endl;
+    }
+}
